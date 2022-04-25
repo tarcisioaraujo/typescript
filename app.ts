35f40d357d840aebb9ccdc62a6902ba10d7385a1
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-04-25 19:13:42 -0300

    Configura para não gerar arquivo final quando houver erros no código.

--- app.ts
+++ app.ts
@@ -2,4 +2,6 @@ let linguagem:string = 'Typescript';
 let texto:string = `Olá ${ linguagem }`;
 let idade:number = 42;
 
+linguagem = '19';
+
 console.log(texto, idade);
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,5 @@
 var linguagem = 'Typescript';
 var texto = "Ol\u00E1 ".concat(linguagem);
 var idade = 42;
+linguagem = '19';
 console.log(texto, idade);
