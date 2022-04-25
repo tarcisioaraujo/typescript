944f10f371ee4cbf06cb570595246fedb1151038
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-04-25 18:45:23 -0300

    Iniciando com typescript.

---
+++ app.js
@@ -0,0 +1,4 @@
+var linguagem = 'Typescript';
+var texto = "Ol\u00E1 ".concat(linguagem);
+var idade = 42;
+console.log(texto, idade);
---
+++ app.ts
@@ -0,0 +1,5 @@
+let linguagem:string = 'Typescript';
+let texto:string = `Olá ${ linguagem }`;
+let idade:number = 42;
+
+console.log(texto, idade);
