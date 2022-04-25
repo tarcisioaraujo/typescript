5f8a8645aeb3fb24ebbea6091d6229cfb065108d
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-04-25 19:19:28 -0300

    Trabalhando com funções.

--- app.ts
+++ app.ts
@@ -4,4 +4,16 @@ let idade:number = 42;
 
 linguagem = '19';
 
+let getPerfil = function(nome:string, idade:number, email:string):void {
+    console.log(nome, idade, email);
+}
+
+function getIdade(idade):string {
+    return idade + " anos";
+}
+
+getPerfil("231", 32, "teste@gmail.com");
+
+let idadeAnos:string = getIdade(42);
+
 console.log(texto, idade);
--- dist/app.js
+++ dist/app.js
@@ -2,4 +2,12 @@ var linguagem = 'Typescript';
 var texto = "Ol\u00E1 ".concat(linguagem);
 var idade = 42;
 linguagem = '19';
+var getPerfil = function (nome, idade, email) {
+    console.log(nome, idade, email);
+};
+function getIdade(idade) {
+    return idade + " anos";
+}
+getPerfil("231", 32, "teste@gmail.com");
+var idadeAnos = getIdade(42);
 console.log(texto, idade);
