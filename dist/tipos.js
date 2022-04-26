24ef822da87637d7794995d9ca226008edb40d40
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2022-04-26 18:38:15 -0300

    Trabalhando com os tipos de variáveis.

---
+++ dist/tipos.js
@@ -0,0 +1,29 @@
+var nome = "Ana";
+var valor = 19;
+var eMaior;
+if (valor > 17) {
+    eMaior = true;
+}
+else {
+    eMaior = false;
+}
+var lista = ["verde", "azul"];
+var listaNumeros = [76, 74, 37];
+var Cor;
+(function (Cor) {
+    Cor[Cor["vermelho"] = 2] = "vermelho";
+    Cor[Cor["azul"] = 3] = "azul";
+    Cor[Cor["laranja"] = 4] = "laranja";
+})(Cor || (Cor = {}));
+;
+var c = Cor.azul;
+console.log(c);
+console.log(Cor[4]);
+var aux;
+aux = "teste";
+aux = 43;
+aux = [43, 34];
+var arrayAny = [43, "teste", {}];
+function listaNomes(lista) {
+    console.log(lista);
+}
---
+++ tipos.ts
@@ -0,0 +1,30 @@
+let nome:string = "Ana";
+let valor:number = 19;
+let eMaior:boolean;
+
+if(valor > 17){
+  eMaior = true;
+}else{
+  eMaior = false;
+}
+
+let lista:string[] = ["verde","azul"];
+let listaNumeros:Array<number> = [76,74,37];
+
+enum Cor {vermelho=2,azul,laranja};
+
+let c:Cor = Cor.azul;
+
+console.log(c);
+console.log(Cor[4]);
+
+let aux:any;
+aux = "teste";
+aux = 43;
+aux = [43,34];
+
+let arrayAny:any[] = [43,"teste",{}];
+
+function listaNomes(lista:string[]):void{
+  console.log(lista);
+}
