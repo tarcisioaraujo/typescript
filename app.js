ea2c95b02acfae1ff50824037cd3875e668a7eda
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-03-25 22:34:07 -0300

    Inicia com arquivos básicos do typescript.

---
+++ app.js
@@ -0,0 +1,4 @@
+var linguagem = 'Typescript';
+var texto = "Ol\u00E1 ".concat(linguagem);
+var idade = 40;
+console.log(texto, idade);
---
+++ app.ts
@@ -0,0 +1,6 @@
+let linguagem:string = 'Typescript'
+let texto:string = `Olá ${ linguagem }`
+let idade:number = 40
+
+
+console.log(texto,idade)
