5908f5daae7605315cd643a3bf00c063722b2cac
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-03-28 17:57:46 -0300

    Configura detecção de erros.

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
-let linguagem:string = 'Typescript'
-let texto:string = `Olá ${ linguagem }`
-let idade:number = 20
+let linguagem:string = 'Typescript';
+let texto:string = `Olá ${ linguagem }`;
+let idade:number = 42;
 
+linguagem = 66;
 
-console.log(texto,idade)
+console.log(texto,idade);
