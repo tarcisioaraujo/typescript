21823fee5a55db98ee5a00240bba4a95b1571405
Tarcísio Silva de Araújo <tarcisio.saraujo@gmail.com>
2025-05-12 18:50:24 -0300

    Trabalhando com classes e interfaces.

--- app.ts
+++ app.ts
@@ -1,30 +1,31 @@
-let nome:string = "Ana";
-let idade:number = 19;
-let eMaior:boolean;
+interface IVeiculo{
+  getTitulo();
+  getRodas();
 
-if(idade > 17){
-  eMaior = true;
-}else{
-  eMaior = false;
 }
 
-let lista:string[] = ["verde","azul"];
-let listaNumeros:Array<number> = [76,74,37];
+class Veiculo implements IVeiculo{
+  constructor(private titulo:string = "Nome Carro", private rodas:number = 2){
+    this.titulo = titulo;
+    this.rodas = rodas;
+  }
 
-enum Cor {vermelho=2,azul,laranja};
+  getTitulo():string{
+    return this.titulo;
+  }
 
-let c:Cor = Cor.azul;
+  getRodas():number{
+    return this.rodas;
+  }
 
-console.log(c);
-console.log(Cor[4]);
-
-let aux:any;
-aux = "teste";
-aux = 43;
-aux = [43,34];
-
-let arrayAny:any[] = [43,"teste",{}];
-
-function listaNomes(lista:string[]):void{
-  console.log(lista);
 }
+
+class Moto extends Veiculo{
+
+}
+
+let camaro:Veiculo = new Veiculo('Camaro');
+
+let honda:Moto = new Moto();
+
+console.log(honda.getTitulo());
--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,42 @@
-var nome = "Ana";
-var idade = 19;
-var eMaior;
-if (idade > 17) {
-    eMaior = true;
-}
-else {
-    eMaior = false;
-}
-var lista = ["verde", "azul"];
-var listaNumeros = [76, 74, 37];
-var Cor;
-(function (Cor) {
-    Cor[Cor["vermelho"] = 2] = "vermelho";
-    Cor[Cor["azul"] = 3] = "azul";
-    Cor[Cor["laranja"] = 4] = "laranja";
-})(Cor || (Cor = {}));
-;
-var c = Cor.azul;
-console.log(c);
-console.log(Cor[4]);
-var aux;
-aux = "teste";
-aux = 43;
-aux = [43, 34];
-var arrayAny = [43, "teste", {}];
-function listaNomes(lista) {
-    console.log(lista);
-}
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Veiculo = /** @class */ (function () {
+    function Veiculo(titulo, rodas) {
+        if (titulo === void 0) { titulo = "Nome Carro"; }
+        if (rodas === void 0) { rodas = 2; }
+        this.titulo = titulo;
+        this.rodas = rodas;
+        this.titulo = titulo;
+        this.rodas = rodas;
+    }
+    Veiculo.prototype.getTitulo = function () {
+        return this.titulo;
+    };
+    Veiculo.prototype.getRodas = function () {
+        return this.rodas;
+    };
+    return Veiculo;
+}());
+var Moto = /** @class */ (function (_super) {
+    __extends(Moto, _super);
+    function Moto() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Moto;
+}(Veiculo));
+var camaro = new Veiculo('Camaro');
+var honda = new Moto();
+console.log(honda.getTitulo());
